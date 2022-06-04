8021830a62bc9257132de00f420c002bdc3e5c68
Knazat <rihk.karlauskis@gmail.com>
2022-06-04 18:37:13 +0300

    pieslipeju kodu

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
   <router-view />
 </template>
+<style lang="scss">
+@import "./styles/reset.scss";
+@import "./styles/App.scss";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,52 @@
 <template>
-  <div>
+  <div class="input-wrapper">
     <input
       v-on:keyup.enter="addTodo"
       placeholder="Add not todo"
       type="text"
       v-model="inputValue"
     />
-    <button @click="addTodo">ADD</button>
+    <button class="button-add" @click="addTodo">ADD</button>
   </div>
-  <div class="single-todo" v-for="(todo, index) in filtered" :key="index">
-    <div>
-      <input
-        :checked="todo.isDone"
-        @click="changeIsDone(index)"
-        type="checkbox"
-        class="button-status"
-      />
-      <span :class="{ taskIsDone: todo.isDone }">{{ todo.name }}</span>
+  <div class="single-todo-box-wrapper" v-for="todo in filtered" :key="todo.id">
+    <div class="single-todo-box">
+      <label class="checkbox-container">
+        <input
+          @click="changeIsDone(todo.id)"
+          :checked="todo.isDone"
+          :unchecked="!todo.isDone"
+          type="checkbox"
+          class="button-status"
+        />
+        <span class="checkmark"></span>
+      </label>
+      <span class="single-todo" :class="{ taskIsDone: todo.isDone }">
+        {{ todo.name }}
+      </span>
     </div>
-    <button class="button-remove" @click="removeTodo(index)">x</button>
+    <button class="button-remove" @click="removeTodo(todo.id)">x</button>
   </div>
-  <div>
-    <button @click="filter = 'all'">All</button>
-    <button @click="filter = 'inProgress'">In progress</button>
-    <button @click="filter = 'completed'">Completed</button>
+  <div class="buttons-filter-box">
+    <button
+      :class="{ selectedFilterButton: filterType === 'all' }"
+      @click="filterType = 'all'"
+    >
+      All
+    </button>
+    <button
+      :class="{ selectedFilterButton: filterType === 'inProgress' }"
+      @click="filterType = 'inProgress'"
+    >
+      In progress
+    </button>
+    <button
+      :class="{ selectedFilterButton: filterType === 'completed' }"
+      @click="filterType = 'completed'"
+    >
+      Completed
+    </button>
   </div>
 </template>
-<style lang="scss">
-@import "../styles/reset.scss";
-@import "../styles/App.scss";
-</style>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -37,20 +54,22 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "HomeView",
   data: () => ({
-    filter: "all",
+    filterType: "all",
     todos: [] as {
       name: string;
       isDone: boolean;
+      id: number;
     }[],
     inputValue: "",
+    initialId: 0,
   }),
   computed: {
     filtered() {
-      if (this.filter === "all") {
+      if (this.filterType === "all") {
         return this.todos;
-      } else if (this.filter === "inProgress") {
+      } else if (this.filterType === "inProgress") {
         return this.todos.filter((todo) => !todo.isDone);
-      } else if (this.filter === "completed") {
+      } else if (this.filterType === "completed") {
         return this.todos.filter((todo) => todo.isDone);
       }
       return this.todos;
@@ -58,22 +77,28 @@ export default defineComponent({
   },
   methods: {
     addTodo() {
-      console.log(this.todos);
       if (this.inputValue) {
         this.todos.push({
           name: this.inputValue,
           isDone: false,
+          id: this.initialId++,
         });
       }
       this.inputValue = "";
     },
     removeTodo(index: number) {
-      this.todos.splice(index, 1);
+      this.todos = this.todos.filter((el) => el.id !== index);
     },
-    changeIsDone(index: number) {
-      if (this.todos[index]) {
-        this.todos[index].isDone = !this.todos[index].isDone;
-      }
+    changeIsDone(todoId: number) {
+      // const index = this.todos.findIndex((todo) => {
+      //   return todoId === todo.id;
+      // });
+      // this.todos[index].isDone = !this.todos[index].isDone;
+      this.todos.forEach((todo) => {
+        if (todo.id === todoId) {
+          todo.isDone = !todo.isDone;
+        }
+      });
     },
   },
 });
